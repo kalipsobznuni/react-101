259115027c118d0f91e153628410a8f4c66b0e8c
kalipsobznuni <kalipso.bznuni14159@gmail.com>
2017-03-11 06:05:27 +0400

    initials

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import uuid from 'uuid';
+import Projects from "./Components/Projects";
+import AddProject from "./Components/AddProject";
+import './App.css';
+
+class App extends Component {
+    constructor(){
+        super();
+      this.state = {
+        projects: [ ]
+      }
+    }
+    componentWillMount(){
+        this.setState({projects: [
+          {
+            id: uuid.v4(),
+            title: "Business Website",
+            category: "Web Design"
+          },
+          {
+              id: uuid.v4(),
+            title: 'Social App',
+            category:'Mobile Development'
+
+          },
+          {
+              id: uuid.v4(),
+            title: 'Ecommerce  Shopping Cart',
+            category: 'Web Development'
+          }
+        ]});
+    }
+
+handleAddProject(project){
+  let projects = this.state.projects;
+  projects.push(project);
+  this.setState({projects:projects});
+}
+
+          render() {
+            return (
+              <div className="App">
+               < AddProject addProject={this.handleAddProject.bind(this)} />
+                <Projects projects={this.state.projects} />
+              </div>
+            );
+          }
+}
+
+export default App;
---
+++ src/Components/AddProject.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import uuid from 'uuid';
+import ProjectItem from "./ProjectItem"
+
+class AddProject extends Component {
+  constructor(){
+    super();
+      newProject:{}
+  }
+
+
+  static defaultProps ={
+    categories: ['Web Design', 'Web Development', 'Mobile Development']
+  }
+handleSubmit(e){
+  if(this.refs.title.value===""){
+    alert('Title is Required');
+  } else{
+    this.setState({newProject:{
+        id: uuid.v4(),
+      title: this.refs.title.value,
+      category: this.refs.category.value
+    }}, function(){
+      this.props.addProject(this.state.newProject)
+    })
+  }
+e.preventDefault();
+}
+  render() {
+    let categoryOptions = this.props.categories.map(category => {
+      return <option key={category} value={category}> {category}</option>
+    });
+    return (
+      <div>
+<h3> Add Project</h3>
+<form onSubmit={this.handleSubmit.bind(this)}>
+<div>
+<label> Title</label>
+<input type ='text' ref='title' />
+</div>
+<div>
+<label> Category</label>
+<select ref ='category'>
+{categoryOptions}
+</select>
+</div>
+<br/>
+<input type='submit' value="Submit"/>
+<br/>
+</form>
+ </div>
+    );
+  }
+}
+
+export default AddProject;
