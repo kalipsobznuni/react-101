b54f1eaa0c02ac2aa61f76d8fe10ebc582830cdd
kalipsobznuni <kalipso.bznuni14159@gmail.com>
2017-03-11 22:43:33 +0400

    add delete handler

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import ProjectItem from "./ProjectItem"
 
 class Projects extends Component {
+    deleteProject(id){
+      this.props.onDelete(id);
+    }
 
   render() {
     let projectItems;
     if (this.props.projects){
       projectItems = this.props.projects.map(project => {
         return(
-          <ProjectItem  key={project.id} project={project} />
+          <ProjectItem  onDelete={this.deleteProject.bind(this)}
+          key={project.id} project={project} />
         );
       });
     }
